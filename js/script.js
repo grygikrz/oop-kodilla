121c1c3b932ac828f43f7ec6a80ef4901dedf0b1
Krzysiek <kgrygiel@gmail.com>
2019-07-08 21:36:04 +0200

    quote script

--- js/script.js
+++ js/script.js
@@ -1,21 +1,43 @@
-function Button(text) {
-	this.text = text || 'Hello';
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var prefix = "https://cors-anywhere.herokuapp.com/";
+
+function getQuote() {
+    fetch(prefix + quoteUrl, { cache: "no-store" })
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(createTweet);
 }
 
-Button.prototype = {
-	create: function() {
-		var self = this;
-		this.element = document.createElement('button');
-		this.element.innerText = this.text;
-		this.element.addEventListener('click', function() {
-			alert(self.text);
-		});
-		document.body.appendChild(this.element);
-	}
+function createTweet(input) {
+    var data = input[0];
+
+    var dataElement = document.createElement('div');
+    dataElement.innerHTML = data.content;
+    var quoteText = dataElement.innerText.trim();
+    var quoteAuthor = data.title;
+
+    if (!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
+    }
+
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+    if (tweetText.length > 140) {
+      getQuote();
+    } else {
+    var tweet = tweetLink + encodeURIComponent(tweetText);
+    document.querySelector('.quote').innerText = quoteText;
+    document.querySelector('.author').innerText = "Author: " + quoteAuthor;
+    document.querySelector('.tweet').setAttribute('href', tweet);
+  }
+  document.querySelector('.tweet').setAttribute('href', tweet);
 }
 
-
-
-var btn1 = new Button('Hello!');
-
-btn1.create();
+document.addEventListener('DOMContentLoaded', function() {
+    getQuote();
+    document.querySelector('.trigger').addEventListener('click', function() {
+        getQuote();
+    });
+});
